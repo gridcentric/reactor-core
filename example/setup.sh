0f31859508fbf181ce51ff3e008ea0bb5cec255a
dscannell <dscannell@gridcentric.com>
2012-03-29 15:34:09 -0400

    Updating the example script to auto-configure the pancake clone.d script.

--- example/setup.sh
+++ example/setup.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-if [ $# != 2 ]
+if [ $# != 3 ]
 then
-    echo "usage: setup.sh example_server path"
+    echo "usage: setup.sh server_user example_server pancake_project_path"
     echo ""
-    echo "e.g. setup.sh openstack@openstack-management.gridcentric.ca ''"
-    echo "e.g. setup.sh dscannell@dscannll-desktop /home/dscannell/projects/gridcentric/cloud/pancake/example"
+    echo "e.g. setup.sh dscannell dscannell-desktop /home/dscannell/projects/gridcentric/pancake"
     exit 1
 fi
 
-EXAMPLE_SERVER=$1
-EXAMPLE_PATH=$2
+EXAMPLE_USER=$1
+EXAMPLE_SERVER=$2
+EXAMPLE_PATH=$3
 
-# setup password
-passwd
+EXAMPLE_BASE=$EXAMPLE_USER@$EXAMPLE_SERVER:$EXAMPLE_PATH
 
 # Acquire the necessary packages
-apt-get install nano zip apache2 libapache2-mod-wsgi python-django curl
+apt-get install -y nano zip apache2 libapache2-mod-wsgi python-django curl
 
 # Install vms-agent
 apt-get install vms-agent
 /etc/init.d/vmsagent start
 
+# Install the pancake agent script
+scp $EXAMPLE_BASE/clone.d/* ./
+cat ./90_pancake | sed "s:PANCAKE_HOST:$EXAMPLE_SERVER:" - > /etc/gridcentric/clone.d/90_pancake
+
+
 # Get the django application
 mkdir -p /web
-scp $EXAMPLE_SERVER:$EXAMPLE_PATH/ip_test.zip /web/ip_test.zip
+scp $EXAMPLE_BASE/example/ip_test.zip /web/ip_test.zip
 unzip /web/ip_test.zip -d /web
 ln -s /web/punchvid/templates /templates
 
-# Get the agent application
-scp $EXAMPLE_SERVER:$EXAMPLE_PATH/../clone.d/* /etc/gridcentric/clone.d
 
 # Configure apache
 cat /etc/apache2/sites-available/default | sed 's:</VirtualHost>:WSGIScriptAlias / /web/punchvid/django.wsgi\n</VirtualHost>:' - > /etc/apache2/sites-available/punchvid
