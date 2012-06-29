dfcc3472c9274cf731674071f1dfb71d996db9ee
Adin Scannell <adin@scannell.ca>
2012-06-29 18:25:29 -0400

    Removing remaining Pancake branding.

--- image/scripts/post.sh
+++ image/scripts/post.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-# Ensure the gridcentric nova service does not start.
-rm -f etc/init/nova-gridcentric.conf
-
 # Make no sure no nginx sites are enabled.
 rm -f etc/nginx/sites-enabled/*
-
-# Enable log rotation.
-cat >/etc/logrotate.d/pancake <<EOF
-/var/log/pancake.log {
-    daily
-    missingok
-    rotate 52
-    compress
-    delaycompress
-    notifempty
-    create 0640 root root
-    sharedscripts
-}
-EOF
