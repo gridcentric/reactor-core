3b90ffb31b6fbbd427921244c5a0ccc80d1e44dc
Rahat Mahmood <rmahmood@gridcentric.ca>
2012-09-25 18:18:23 -0400

    Fixing type error, metric_ips is a list.

--- gridcentric/pancake/manager.py
+++ gridcentric/pancake/manager.py
@@ -773,7 +773,8 @@ class ScaleManager(object):
                     # This should be a dictionary { "name" : (weight, value) }
                     ip_metrics = json.loads(ip_metrics)
                     metrics.append(ip_metrics)
-                    metric_ips.add(ip_address)
+                    if not ip_address int metric_ips:
+                        metric_ips.append(ip_address)
                     if self.metric_indicates_active(ip_metrics):
                         active_connections.append(ip_address)
                 except ValueError:
@@ -790,7 +791,8 @@ class ScaleManager(object):
                             # As above, this should be a dictionary.
                             ip_metrics = json.loads(ip_metrics)
                             metrics.append(ip_metrics)
-                            metric_ips.add(ip_address)
+                            if not ip_address in metric_ips:
+                                metric_ips.append(ip_address)
                             if self.metric_indicates_active(ip_metrics):
                                 active_connections.append(ip_address)
                         except ValueError:
