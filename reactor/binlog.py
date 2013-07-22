18d17c6615636dae7fd7119edec4c37a4c9f8378
Tim Smith <tsmith@gridcentric.com>
2013-07-21 20:17:15 -0700

    Unit testing: Binary log (binlog)
    This patch adds unit testing for the binlog module. The unit
    tests aren't run by default - support for that will be added
    once we get our Reactor unit testing strategy solidified.
    
    This patch makes use of the unittest and mock modules, which
    will soon become part of the Python standard. For 2.6 etc,
    you must install (via pip) mock and unittest2.
    
    Change-Id: I3cee344b4c98f8ab75a502dc211ac66d11e2eab0

--- reactor/binlog.py
+++ reactor/binlog.py
@@ -13,8 +13,8 @@ class BinaryLogRecord(object):
 #   u16 record type
 #   u32 arg1
 #   u32 arg2
-STRUCT_FMT = '<dHHLL'
-STRUCT_SIZE = struct.calcsize(STRUCT_FMT)
+ENTRY_FMT = '<dHHll'
+ENTRY_SIZE = struct.calcsize(ENTRY_FMT)
 
 class BinaryLog(object):
 
@@ -31,8 +31,10 @@ class BinaryLog(object):
 
     def __init__(self, size, record_types=None,
                  store_cb=None, retrieve_cb=None):
+        if size < ENTRY_SIZE:
+            raise ValueError("Size must be greater than %d" % ENTRY_SIZE)
         self.size = size
-        self.num_entries = self.size / STRUCT_SIZE
+        self.num_entries = self.size / ENTRY_SIZE
         self.type_list = []
         self.type_map = {}
         if record_types:
@@ -62,21 +64,21 @@ class BinaryLog(object):
         if not self.data or len(self.data) != self.size:
             self.data = bytearray(self.size)
         self.buffer = buffer(self.data)
-        self.find_pointer()
+        self.__find_pointer()
 
-    def extract_entry(self, index):
-        return struct.unpack_from(STRUCT_FMT, self.buffer, index * STRUCT_SIZE)
+    def __extract_entry(self, index):
+        return struct.unpack_from(ENTRY_FMT, self.buffer, index * ENTRY_SIZE)
 
-    def create_entry(self, index, ts, sev, code, arg1, arg2):
-        struct.pack_into(STRUCT_FMT, self.data, index * STRUCT_SIZE,
+    def __create_entry(self, index, ts, sev, code, arg1, arg2):
+        struct.pack_into(ENTRY_FMT, self.data, index * ENTRY_SIZE,
                         ts, sev, code, arg1, arg2)
 
-    def find_pointer(self):
+    def __find_pointer(self):
         max_ts = 0
         max_ts_ent = 0
         # Look for the latest non-null entry
         for i in range(self.num_entries):
-            (ts, sev, code, arg1, arg2) = self.extract_entry(i)
+            (ts, sev, code, arg1, arg2) = self.__extract_entry(i)
             if ts > max_ts:
                 max_ts = ts
                 max_ts_ent = i
@@ -85,56 +87,50 @@ class BinaryLog(object):
         if max_ts > 0:
             # Use the following slot as the write pointer
             self.pointer = max_ts_ent
-            self.pointer = self.next_pointer(self.pointer)
+            self.pointer = self.__next_pointer(self.pointer)
         # Else,
         else:
             # Start at zero.
             self.pointer = 0
 
-    def next_pointer(self, current):
+    def __next_pointer(self, current):
         current += 1
         if current >= self.num_entries:
             current = 0
         return current
 
-    def prev_pointer(self, current):
-        current -= 1
-        if current < 0:
-            current  = self.num_entries - 1
-        return current
-
-    def log_raw(self, sev, code, *args):
+    def __log_raw(self, sev, code, *args):
         # Gather entry fields
         ts = time.time()
         arg1 = len(args) >= 1 and args[0] or 0
         arg2 = len(args) >= 2 and args[1] or 0
 
         # Write entry
-        self.create_entry(self.pointer, ts, sev, code, arg1, arg2)
+        self.__create_entry(self.pointer, ts, sev, code, arg1, arg2)
 
         # Advance pointer
-        self.pointer = self.next_pointer(self.pointer)
+        self.pointer = self.__next_pointer(self.pointer)
 
         # Store log
         if self.store_cb:
             self.store_cb(self.data)
 
-    def log(self, sev, rectype, *args):
+    def __log(self, sev, rectype, *args):
         # Get code for rectype (or throw exception)
         code = self.type_map[rectype]
         # Log raw entry
-        return self.log_raw(sev, code, *args)
+        return self.__log_raw(sev, code, *args)
 
     def info(self, rectype, *args):
-        self.log(self.INFO, rectype, *args)
+        self.__log(self.INFO, rectype, *args)
 
     def warn(self, rectype, *args):
-        self.log(self.WARN, rectype, *args)
+        self.__log(self.WARN, rectype, *args)
 
     def error(self, rectype, *args):
-        self.log(self.ERROR, rectype, *args)
+        self.__log(self.ERROR, rectype, *args)
 
-    def get_raw(self, since=None, num=None):
+    def __get_raw(self, since=None, num=None):
         entries = []
 
         # If starting point is unspecified, return
@@ -145,7 +141,7 @@ class BinaryLog(object):
         # If limit is unspecified, return all
         # available entries
         if not(num):
-            num = self.size / STRUCT_SIZE
+            num = self.size / ENTRY_SIZE
 
         # Return entries, in chronological order, starting
         # from the slot after the last written entry
@@ -154,14 +150,14 @@ class BinaryLog(object):
         # Pull the requested amount:
         while len(entries) < num:
             # Extract an entry
-            (ts, sev, code, arg1, arg2) = self.extract_entry(pointer)
+            (ts, sev, code, arg1, arg2) = self.__extract_entry(pointer)
             # If it's non-null and after the requested timestamp,
             if code != 0 and ts > since:
                 # Save it
                 entries.append((ts, sev, code, arg1, arg2))
 
             # Advance the pointer
-            pointer = self.next_pointer(pointer)
+            pointer = self.__next_pointer(pointer)
 
             # Quit if we've looped around the log
             if pointer == orig_pointer:
@@ -169,7 +165,7 @@ class BinaryLog(object):
 
         return entries
 
-    def pretty_print_entry(self, entry):
+    def __pretty_print_entry(self, entry):
         (ts, sev, code, arg1, arg2) = entry
         # Get original record type
         if code > 0 and code <= len(self.type_list):
@@ -191,10 +187,10 @@ class BinaryLog(object):
 
     def get(self, since=None, num=None):
         # Get raw entries
-        raw_entries = self.get_raw(since, num)
+        raw_entries = self.__get_raw(since, num)
 
         # Convert to human readable entries
-        entries = map(self.pretty_print_entry, raw_entries)
+        entries = map(self.__pretty_print_entry, raw_entries)
 
         # Return pretty-printed list
         return entries
