acad0f1e905d701906e5135de655eacf6a1210db
Tim Smith <tsmith@gridcentric.com>
2013-07-31 17:30:30 -0700

    Add endpoint templates.
    This patch adds a templating system for endpoint
    configuration, implemented as an ordered JSON
    structure that describes sections, configuration
    items, and hidden values, along with appropriate
    defaults. An initial RDP template is provided.
    
    Additionally, this patch removes the potentially
    hazardous "new manager" functionality from the
    GUI sidebar, as well as the "clone endpoint"
    functionality.
    
    Change-Id: I225e27988ebad701187976fe818cfaf227dbadda

---
+++ reactor/admin/assets/templates.js
@@ -0,0 +1,43 @@
+ENDPOINT_TEMPLATES = {
+    "rdp" : {
+            "description" : "Remote Desktop Protocol",
+            "components" : [
+                {
+                    "name" : "loadbalancer", "description" : "RDP Settings", "items" : [
+                        { "item" : "loadbalancer:rdp:username" },
+                        { "item" : "loadbalancer:rdp:password" },
+                        { "item" : "loadbalancer:rdp:domain" },
+                        { "item" : "loadbalancer:rdp:host" },
+                        { "item" : "loadbalancer:rdp:template" },
+                        { "item" : "loadbalancer:rdp:reconnect" }
+                    ]
+                },
+                {
+                    "name" : "cloud", "description" : "Cloud Settings", "items" : [
+                        { "item" : "cloud:osvms:auth_url" },
+                        { "item" : "cloud:osvms:username" },
+                        { "item" : "cloud:osvms:tenant_name" },
+                        { "item" : "cloud:osvms:password" },
+                        { "item" : "cloud:osvms:instance_id" },
+                        { "item" : "cloud:osvms:security_groups"},
+                    ]
+                },
+                {
+                    "name" : "scaling", "description" : "Auto-scaling", "items" : [
+                        { "item" : "scaling:min_instances", "default" : "1" },
+                        { "item" : "scaling:max_instances", "default" : "1" },
+                        { "item" : "scaling:rules" }
+                    ]
+                },
+                {
+                    "items" : [
+                        { "item" : "endpoint:url", "default" : "rdp://" },
+                        { "item" : "endpoint:port", "default" : "3389" },
+                        { "item" : "endpoint:cloud", "default" : "osvms" },
+                        { "item" : "endpoint:loadbalancer", "default" : "rdp" },
+                        { "item" : "endpoint:template", "default" : "rdp" }
+                    ]
+                }
+            ]
+        }
+}
